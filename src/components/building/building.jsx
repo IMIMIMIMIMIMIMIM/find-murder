4087a8b3457c6301bba43bc2212edcfcfca178ce
IMIMIMIMIMIMIMIM <ism1124@naver.com>
2023-02-11 18:42:02 +0900

    feat, fix : door img fix, floor, room hint done #15

--- src/components/building/building.jsx
+++ src/components/building/building.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Hint from "../hint/hint";
 
 const Building = () => {
   const navigate = useNavigate();
